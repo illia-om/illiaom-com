5706bab188b53450eb77b7da3b386ff61261fa6e
illia <illia.omelchenko@icloud.com>
2022-10-14 16:07:18 +0100

    change fontn & path rendering in header

--- components/header.tsx
+++ components/header.tsx
@@ -11,22 +11,25 @@ type HeaderProps = {
 const Header = ({ path }: HeaderProps) => {
     return (
         <header className={styles.header}>
+            <div className={`${utilStyles.container} ${styles.container}`}>
                 {path ? (
                     <>
                         <h1 className={utilStyles.headingXl}>
                             <Link href="/">
-                                <a className={utilStyles.colorInherit}>{illia}</a>
+                                <a className={utilStyles.colorInherit}>@illia_om</a>
                             </Link>
                         </h1>
+                        <h1 className={utilStyles.headingXl}>/{path.join('/')}</h1>
                     </>
 
                 ) : (
                     <>
                         <h1 className={utilStyles.headingXl}>{illia}</h1>
+                        <h1 className={`${utilStyles.headingXl} ${styles.underline} ${utilStyles.deleteCharSpacing}`}>{Array(1000).fill('_')}</h1>
+                        <h1 className={utilStyles.headingXl}>om</h1>
                     </>
                 )}
-                <h1 className={utilStyles.headingXl + ' ' + styles.underline + ' ' + utilStyles.deleteCharSpacing}>{Array(1000).fill('_')}</h1>
-                <h1 className={utilStyles.headingXl}>om</h1>
+            </div>
         </header>
     );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inconsolata"
         />
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono"
+        />
       </Head>
       <body>
         <Main />
